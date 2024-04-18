adca97d4e8a10343e4d78faa792b8b68e0d96be9
Isabela <106828051+IsabelaPO@users.noreply.github.com>
2024-04-18 18:06:39 +0100

    Add files via upload

---
+++ es24-62/backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/dto/EnrollmentDto.java
@@ -0,0 +1,66 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.dto;
+
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.domain.Enrollment;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler;
+
+public class EnrollmentDto {
+    private Integer id;
+    private String motivation;
+
+    private String enrollmentDateTime;
+
+    private String volunteer_name;
+
+    private Integer volunteerId;
+
+    private Integer activityId;
+
+
+    public EnrollmentDto() {}
+
+    public EnrollmentDto(Enrollment enrollment) {
+        this.id = enrollment.getId();
+        this.motivation = enrollment.getMotivation();
+        this.enrollmentDateTime = DateHandler.toISOString(enrollment.getEnrollmentDateTime());
+        this.activityId = enrollment.getActivity().getId();
+        this.volunteer_name = enrollment.getVolunteer().getName();
+        this.volunteerId = enrollment.getVolunteer().getId();
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMotivation() {
+        return motivation;
+    }
+
+    public void setMotivation(String motivation) {
+        this.motivation = motivation;
+    }
+
+    public String getEnrollmentDateTime() {
+        return enrollmentDateTime;
+    }
+
+    public void setEnrollmentDateTime(String enrollmentDateTime) {
+        this.enrollmentDateTime = enrollmentDateTime;
+    }
+
+    public String getVolunteer_name(){
+        return this.volunteer_name;
+    }
+
+    public Integer getVolunteerId(){
+        return this.volunteerId;
+    }
+    public Integer getActivityId(){
+        return this.activityId;
+    }
+}
